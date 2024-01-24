92dd4a5bf691b52a93329800a6a981bac81201b7
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2024-01-24 21:57:57 +0100

    alapok

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+from level import Level
+from settings import *
+
+screen = pygame.display.set_mode((widht, height))
+clock = pygame.time.Clock()
+level = Level(level_map, screen)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill("black")
+
+    level.run()
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
---
+++ settings.py
@@ -0,0 +1,19 @@
+level_map = [
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    " XX    XXX             XX   ",
+    " XX P                       ",
+    " XXXX         XX         XX ",
+    " XXXX       XX              ",
+    " XX    X  XXXX     XX  XX   ",
+    "       X  XXXX     XX  XXX  ",
+    "    XXXX  XXXXXX   XX  XXXX ",
+    " XXXXXXX  XXXXXX   XX  XXXX ",
+    " XXXXXXX  XXXXXX   XX  XXXX ",
+]
+
+tile_size = 64
+widht, height = 1900, len(level_map) * tile_size
