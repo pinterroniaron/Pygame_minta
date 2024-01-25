36ffcba33ec1c7a89dabd8e2b47e3a3b6a61d4b4
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2024-01-25 20:53:18 +0100

    idle animáció + javítások

--- player.py
+++ player.py
@@ -1,10 +1,15 @@
 import pygame
 
+from support import import_folder
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.image.load("images/duck_idle_1.png")
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.04
+        self.image = self.animations["idle"][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
 
         self.direction = pygame.math.Vector2(0, 0)
@@ -12,6 +17,27 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
 
+    def import_character_assets(self):
+        character_path = "graphics/character/"
+        self.animations = {
+            "idle": [],
+            "run": [],
+            "jump": [],
+            "fall": [],
+        }
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations["idle"]
+
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -28,11 +54,11 @@ class Player(pygame.sprite.Sprite):
 
     def apply_gravity(self):
         self.direction.y += self.gravity
-        self.rect.y += self.direction.y 
+        self.rect.y += self.direction.y
 
     def jump(self):
         self.direction.y = self.jump_speed
 
-
     def update(self):
         self.get_input()
+        self.animate()
