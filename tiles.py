b2455ff1b0929a930967517624d852bd826478e4
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2024-01-26 08:43:25 +0100

    első földblock

--- tiles.py
+++ tiles.py
@@ -4,8 +4,7 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos: int, size: int):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill("green")
+        self.image = pygame.image.load("graphics/map/blocks/dirt.png")
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x_shift):
