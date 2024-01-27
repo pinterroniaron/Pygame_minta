a56ae5e435c00ac366df0180996e3260a3d4f7bf
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2024-01-27 15:31:07 +0100

    animációk kész

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0
+        self.status = "idle"
         self.animation_speed = 0.35
         self.image = self.animations["idle"][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
@@ -16,6 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.speed = 8
         self.gravity = 0.8
         self.jump_speed = -16
+        self.facing_left = True
 
     def import_character_assets(self):
         character_path = "graphics/character/"
@@ -31,21 +33,40 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
 
     def animate(self):
-        animation = self.animations["run"]
+        animation = self.animations[self.status]
 
         self.frame_index += self.animation_speed
         if self.frame_index >= len(animation):
             self.frame_index = 0
 
-        self.image = animation[int(self.frame_index)]
+        image = animation[int(self.frame_index)]
+
+        if self.facing_left:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image, True, False)
+            self.image = flipped_image
+
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = "jump"
+        elif self.direction.y > 1:
+            self.status = "fall"
+        else:
+            if self.direction.x != 0:
+                self.status = "run"
+            else:
+                self.status = "idle"
 
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.facing_left = False
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_left = True
         else:
             self.direction.x = 0
 
@@ -61,4 +82,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.get_status()
         self.animate()
--- support.py
+++ support.py
@@ -1,12 +1,12 @@
 from os import walk
 import pygame
 
-def import_folder(path):
+def import_folder(path: str):
     surface_list = []
 
     for _, __, img_files in walk(path):
         for image in img_files:
-            full_path = path + "/" + image
+            full_path: str = path + "/" + image
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
 
