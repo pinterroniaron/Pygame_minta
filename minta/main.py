c426a7ac1602c76d3ce14a18e212512b6e22af8e
szephalmiaron <szephalmi.aron@students.jedlik.eu>
2024-02-06 16:01:39 +0100

    minta befejezés

--- minta/main.py
+++ minta/main.py
@@ -1,5 +1,5 @@
-from contextlib import redirect_stderr
 import pygame
+import sys
 
 WIDTH = 1280
 HEIGHT = 620
@@ -11,9 +11,8 @@ clock = pygame.time.Clock()
 black = (0, 0, 0)
 red = (255, 0, 0)
 obstacle_colour = black
-kacsa = pygame.image.load('minta\kacsa.png').convert_alpha()
-kacsa_x = WIDTH / 2
-kacsa_y = HEIGHT / 2
+kacsa = pygame.image.load('minta/kacsa.png').convert_alpha()
+kacsa_rect = kacsa.get_rect(center=(WIDTH / 2, HEIGHT / 2))
 
 running = True
 while running:
@@ -21,29 +20,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # Irányítás egérrel
+            kacsa_rect.center = event.pos
 
+    # Irányítás billentyűzettel
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT] and kacsa_rect.right <= WIDTH:
-        kacsa = pygame.image.load('minta\kacsaflip.png')
-        kacsa_x += SPEED
+        kacsa = pygame.image.load('minta/kacsaflip.png')
+        kacsa_rect.x += SPEED
     elif keys[pygame.K_LEFT] and kacsa_rect.left >= 0:
-        kacsa = pygame.image.load('minta\kacsa.png')
-        kacsa_x -= SPEED
-
+        kacsa = pygame.image.load('minta/kacsa.png')
+        kacsa_rect.x -= SPEED
     if keys[pygame.K_UP] and kacsa_rect.top >= 0:
-        kacsa_y -= SPEED
+        kacsa_rect.y -= SPEED
     elif keys[pygame.K_DOWN] and kacsa_rect.bottom <= HEIGHT:
-        kacsa_y += SPEED
+        kacsa_rect.y += SPEED
 
-    kacsa_rect = kacsa.get_rect(center=(kacsa_x, kacsa_y))
-
+    # Ütközés
     obstacle = pygame.draw.rect(screen, obstacle_colour, (100, 100, 200, 200))
     if kacsa_rect.colliderect(obstacle):
         obstacle_colour = red
     else:
         obstacle_colour = black
+
     screen.blit(kacsa, kacsa_rect)
     pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
+sys.exit()
